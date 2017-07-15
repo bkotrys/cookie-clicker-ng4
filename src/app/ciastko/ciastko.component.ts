2d65af1fce5a0fc5eac2c0fc56b11cf601776dd7
Mateusz Kupiniak <mkupiniak@gmail.com>
2017-07-15 10:44:27 +0200

    initial ciastko component

---
+++ src/app/ciastko/ciastko.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ciastko',
+  templateUrl: './ciastko.component.html',
+  styleUrls: ['./ciastko.component.css']
+})
+export class CiastkoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
