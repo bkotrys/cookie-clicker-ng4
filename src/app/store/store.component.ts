6c00532f9b2de61bd757c79dc2fc47f70fa59df3
Piotr Gąsiorowski <gasiorowski.piotr@windowslive.com>
2017-07-15 14:16:41 +0200

    Can buy new clickers.

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { ClickService } from '../services/click.service';
 
 @Component({
   selector: 'app-store',
@@ -9,8 +10,23 @@ export class StoreComponent implements OnInit {
   public items = [
   	{ name: "cursor" },
   	{ name: "grandma" }
-  ]		
+  ]
+  
+  private canBuyNewClicker: boolean = false;
+
+  constructor(@Inject(ClickService) private service: ClickService ) {
+    this.service.CanBuyNewClicker.subscribe((can) => {
+      this.canBuyNewClicker = can;
+    });
+  }
+
   ngOnInit() {
+  }
+
+  buyCursor() {
+    if(this.canBuyNewClicker) {
+      this.service.buyClicker();
+    }    
   }
 
 }
